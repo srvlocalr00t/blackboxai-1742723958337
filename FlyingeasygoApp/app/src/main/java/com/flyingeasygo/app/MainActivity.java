70a4ccb0fb81b77b35036dc8064d4ee412e9c3fa
srvlocalr00t <saurabhebay1@gmail.com>
2025-03-23 15:33:45 +0530

    Add FlyingeasygoApp/app/src/main/java/com/flyingeasygo/app/MainActivity.java 1742724224059

---
+++ FlyingeasygoApp/app/src/main/java/com/flyingeasygo/app/MainActivity.java
@@ -0,0 +1,27 @@
+package com.flyingeasygo.app;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://flyingeasygo.com");
+    }
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
